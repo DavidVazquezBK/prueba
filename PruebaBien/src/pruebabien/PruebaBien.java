25a77c5e23f3f9b506313bfa59d9f192114347fd
DavidVazquez <burnkillreach@gmail.com>
2017-10-04 18:37:28 -0500

    Iniciado

---
+++ PruebaBien/src/pruebabien/PruebaBien.java
@@ -0,0 +1,19 @@
+/*
+ * Coded by David Vazquez using NetBeans.
+ */
+package pruebabien;
+
+/**
+ *
+ * @author BurnKill
+ */
+public class PruebaBien {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
